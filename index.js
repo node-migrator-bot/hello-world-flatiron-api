32c480a75b7c8bc97cedcc9b6defc3c24be2efbe
Joshua Holbrook <josh.holbrook@gmail.com>
2012-03-23 20:30:51 -0700

    pal

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(flatiron.plugins.http,
 });
 
 app.router.get('/', function () {
-  this.res.json({"hello": "buddy"});
+  this.res.json({"hello": "pal"});
 });
 
 app.start(3000, function () { console.log({"flatiron": "ok"}); });
